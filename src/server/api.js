a753971a4409cfa7910c13580f99af66e57f4f95
Admin <riverflow1928@yandex.ru>
2019-08-02 01:56:40 +0500

    create-react-app added

--- src/server/api.js
+++ src/server/api.js
@@ -19,7 +19,7 @@ mongoose.connect(mongoDB, { useNewUrlParser: true })
     .then(db => console.log("Connected to database successfully!"))
     .catch(err => console.log("Unable to connect to the database. Error:", err));
 
-app.use(express.static("../front"));
+app.use(express.static("../front/build"));
 app.use(cookieParser());
 
 app.use("/users", createUser);
